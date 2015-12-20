e900d6fd9546ffc758eca30fb19a20ccbf0b34b9
Kris Siepert <m@kris.cool>
2015-12-20 22:35:28 +0100

    added sftpoutput

---
+++ src/Output/SFTPOutput.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace kriskbx\wyn\Output;
+
+use League\Flysystem\Sftp\SftpAdapter;
+
+class SFTPOutput extends FlySystemOutput
+{
+    /**
+     * Constructor.
+     *
+     * @param string $path
+     * @param string $host
+     * @param string $username
+     * @param string $password
+     * @param string $privateKey
+     * @param int    $timeout
+     * @param int    $port
+     * @param array  $exclude
+     * @param bool   $ignore
+     */
+    public function __construct($path, $host, $username, $password = null, $privateKey = null, $timeout = 10, $port = 21, $exclude = [], $ignore = true)
+    {
+        parent::__construct($exclude, $ignore);
+
+        $adapter = new SftpAdapter([
+            'host' => $host,
+            'port' => $port,
+            'username' => $username,
+            'password' => $password,
+            'privateKey' => $privateKey,
+            'root' => $path,
+            'timeout' => $timeout,
+            'directoryPerm' => 0755,
+        ]);
+
+        $this->setFilesystem($adapter);
+    }
+}
